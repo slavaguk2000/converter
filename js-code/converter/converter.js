efbdfb131280d5df769bd0c89dcdac830f362e80
Viacheslav <slavaguk2000@gmail.com>
2020-10-30 12:56:47 +0300

    indicate about result

--- js-code/converter/converter.js
+++ js-code/converter/converter.js
@@ -1,7 +1,6 @@
 var converWorker = new Worker("js-code/converter/convert_worker.js");
 converWorker.onmessage = function(msg){
     if (msg.data == "init"){
-        console.log("init")
         document.getElementById("drop-area").className = "visible_elem";
     }  
 }
@@ -9,10 +8,12 @@ converWorker.onmessage = function(msg){
 start_convert_worker = function(array, inFormat, outFormat, onConvert, onShow){     
         converWorker.postMessage({array: array, in: inFormat, out: outFormat})
         converWorker.onmessage = function(event){
-            if(event.data.type == "convert"){
-                onConvert(event.data.array)
+            if (event.data == null){
+                onConvert(null);
+            } else if(event.data.type == "convert"){
+                onConvert(event.data.array);
             } else if (event.data.type == "show"){
-                onShow(event.data.array)
+                onShow(event.data.array);
             }
         }
 }

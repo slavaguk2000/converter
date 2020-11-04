368cea4c5eaf4bb8e059a858651c045671f75be5
Viacheslav <slavaguk2000@gmail.com>
2020-11-04 23:05:03 +0300

    add spinner

--- js-code/converter/converter.js
+++ js-code/converter/converter.js
@@ -1,16 +1,29 @@
 var converWorker = new Worker("js-code/converter/convert_worker.js");
 converWorker.onmessage = function(msg){
     if (msg.data == "init"){
-        document.getElementById("drop-area").className = "visible_elem  white-frame";
+        document.getElementById("drop-area").className = "white-frame";
     }  
 }
 
-start_convert_worker = function(array, inFormat, outFormat, onConvert, onShow){     
+function change_spinner_visibility(visible) {
+    let invisible_class = 'invisible_elem';
+    let spinner = document.getElementById("convert-spinner");
+    if(visible){
+        spinner.classList.remove(invisible_class)
+    } else {
+        spinner.classList.add(invisible_class)
+    }
+}
+
+start_convert_worker = function(array, inFormat, outFormat, onConvert, onShow){    
+        change_spinner_visibility(true);
         converWorker.postMessage({array: array, in: inFormat, out: outFormat})
         converWorker.onmessage = function(event){
             if (event.data == null){
+                change_spinner_visibility(false);
                 onConvert(null);
             } else if(event.data.type == "convert"){
+                change_spinner_visibility(false);
                 onConvert(event.data.array);
             } else if (event.data.type == "show"){
                 onShow(event.data.array);
--- js-code/files/upload.js
+++ js-code/files/upload.js
@@ -1,16 +1,16 @@
 
-let dropArea = document.getElementById('drop-area')
-;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+let dropArea = document.getElementById('drop-area');
+['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
     dropArea.addEventListener(eventName, preventDefaults, false)
 })
 function preventDefaults (e) {
     e.preventDefault()
     e.stopPropagation()
-}
-;['dragenter', 'dragover'].forEach(eventName => {
+};
+['dragenter', 'dragover'].forEach(eventName => {
     dropArea.addEventListener(eventName, highlight, false)
-})
-;['dragleave', 'drop'].forEach(eventName => {
+});
+['dragleave', 'drop'].forEach(eventName => {
     dropArea.addEventListener(eventName, unhighlight, false)
 })
 function highlight(e) {
@@ -21,9 +21,7 @@ function unhighlight(e) {
 }
 dropArea.addEventListener('drop', handleDrop, false)
 function handleDrop(e) {
-    let dt = e.dataTransfer
-    let files = dt.files
-    handleFiles(files)
+    handleFiles(e.dataTransfer.files)
 }
 function handleFiles(files) {
     ([...files]).forEach(uploadFile)
